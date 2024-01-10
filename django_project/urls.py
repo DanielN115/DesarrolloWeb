c422a1d48fd3d7555e815e9a47f6eec7693cd14a
johnym <johnyamg20@gmail.com>
2024-01-10 17:35:06 -0500

    'ch6'

--- django_project/urls.py
+++ django_project/urls.py
@@ -18,6 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include("pages.urls")), # new
+    # Django admin
+    path("admin/", admin.site.urls),
+    # User management
+    path("accounts/", include("django.contrib.auth.urls")), # new
+    # Local apps
+    path("", include("pages.urls")),
 ]
+
